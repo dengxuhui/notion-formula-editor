1b2bf4e76f6fcbd50219bc8b4e8ee2fa7f236c5e
dengxuhui <419270388@qq.com>
2023-05-16 17:17:55 +0800

    使用URP，初始化一个简易场景 图标设置，启动场景 删除xnode 使用UnityRuntimeEditor作为基础框架

---
+++ notion-formula-editor/Assets/AssetsPackage/Scripts/NodeEditor/NotionFormulaNodeEditor.cs
@@ -0,0 +1,70 @@
+using RuntimeNodeEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace NotionFormulaEditor
+{
+    /// <summary>
+    /// Notion公式编辑器
+    /// </summary>
+    public class NotionFormulaNodeEditor : NodeEditor
+    {
+        public override void StartEditor(NodeGraph graph)
+        {
+            base.StartEditor(graph);
+
+            Events.OnGraphPointerClickEvent += OnGraphPointerClick;
+            Events.OnNodePointerClickEvent += OnNodePointerClick;
+            Events.OnConnectionPointerClickEvent += OnNodeConnectionPointerClick;
+            Events.OnSocketConnect += OnConnect;
+            
+            // Graph.
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="eventData"></param>
+        private void OnGraphPointerClick(PointerEventData eventData)
+        {
+            Debug.Log("OnGraphPointerClick");
+            switch (eventData.button)
+            {
+                case PointerEventData.InputButton.Right:
+                {
+                    var ctx = new ContextMenuBuilder();
+                    var ctxData = ctx.Add("test", () => { Debug.Log("TestGraphClick"); }).Build();
+
+                    SetContextMenu(ctxData);
+                    DisplayContextMenu();
+                }
+                    break;
+                case PointerEventData.InputButton.Left:
+                {
+                    CloseContextMenu();
+                }
+                    break;
+            }
+        }
+
+        private void OnNodePointerClick(Node node, PointerEventData eventData)
+        {
+            Debug.Log("OnNodePointerClick");
+        }
+
+        private void OnNodeConnectionPointerClick(string connId, PointerEventData eventData)
+        {
+            Debug.Log("OnNodeConnectionPointerClick");
+        }
+
+        /// <summary>
+        /// 连接响应
+        /// </summary>
+        /// <param name="arg1"></param>
+        /// <param name="arg2"></param>
+        private void OnConnect(SocketInput arg1, SocketOutput arg2)
+        {
+            Graph.drawer.SetConnectionColor(arg2.connection.connId, Color.green);
+        }
+    }
+}
